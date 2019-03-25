6d0830ac2e91e974b97c284780159727a27324ee
Felix Mendoza <mendoza.felix@outlook.com>
2019-03-25 17:25:18 -0400

    creating weather app

---
+++ src/components/Weather.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+
+class Weather extends React.Component {
+   render() {
+
+	return (
+		<div>
+		Weather component
+		</div>
+     );
+    }
+
+
+  };
+
+export default Weather;
